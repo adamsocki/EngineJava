07b820de5189bede77ee423a29d092c555b9f9fe
socki <socki@LAPTOP-DGI9IL02>
2022-12-19 14:42:05 -0600

    Engine - Create Window

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,22 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DisplayManager.createDisplay();
+		
+		while(!Display.isCloseRequested())
+		{
+			DisplayManager.updateDisplay();
+		}
+		
+		DisplayManager.closeDisplay();
+	}
+
+}
