7c2ea7893fcb160910244154a52e1c0aa0975625
socki <socki@LAPTOP-DGI9IL02>
2022-12-21 18:46:44 -0600

    Refactor Renderer to MasterRenderer

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,5 +1,9 @@
 package engineTester;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -10,6 +14,7 @@ import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.MasterRenderer;
 import renderEngine.OBJParser;
 import renderEngine.Renderer;
 import shaders.StaticShader;
@@ -21,8 +26,8 @@ public class MainGameLoop {
 
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
-		StaticShader shader = new StaticShader();
-		Renderer renderer = new Renderer(shader);
+		//StaticShader shader = new StaticShader();
+		//Renderer renderer = new Renderer(shader);
 			
 		//RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
 		RawModel model = OBJParser.loadObjModel("dragon", loader);
@@ -32,25 +37,52 @@ public class MainGameLoop {
 		texture.setShineDamper(10);
 		texture.setReflectivity(1);
 		
-		Entity entity = new Entity(staticModel, new Vector3f(0,-4,-25),0,0,0,1);
+		//Entity entity = new Entity(staticModel, new Vector3f(0,-4,-25),0,0,0,1);
 		Light light = new Light(new Vector3f(0,0,-20), new Vector3f(1,1,1));
 		
+		
+		
 		Camera camera = new Camera();
+		// create entities
+		List<Entity> allDragons = new ArrayList<Entity>();
+		Random random = new Random();
+		
+		for (int i = 0; i < 43; i++)
+		{
+			float x = random.nextFloat() * 100 - 50;
+			float y = random.nextFloat() * 100 - 50;
+			float z = random.nextFloat() * -300;
+			allDragons.add(new Entity(
+					staticModel, new Vector3f(x, y, z), 
+					random.nextFloat() * 180f, random.nextFloat() * 180f, 
+					0f, 1f));
+ 		}
+		
+		MasterRenderer renderer = new MasterRenderer();
 		
 		while(!Display.isCloseRequested()){
+			
+			
 			//game logic
 			//entity.increaseRotation(0.08f, 0.08f, -0.08f);
-			renderer.prepare();
+			//renderer.prepare();
 			camera.move();
-			shader.start();
-			shader.loadLight(light);
-			shader.loadViewMatrix(camera);
-			renderer.render(entity,shader);
-			shader.stop();
+			
+			for (Entity dragon:allDragons)
+			{
+				renderer.processEntity(dragon);
+			}
+			//shader.start();
+			//shader.loadLight(light);
+			//shader.loadViewMatrix(camera);
+			//renderer.render(entity,shader);
+			//shader.stop();
+			renderer.render(light, camera);
 			DisplayManager.updateDisplay();		
 		}
 
-		shader.cleanUp();
+		//shader.cleanUp();
+		renderer.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
